4558faa3b8c5526439452682e90d827efc6ff6ab
juniorshait <juniorshait@gmail.com>
2022-05-02 08:54:49 -0700

    Escreva um método que verifica se o caractere está em maiúsculo.

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,9 @@
+public class Caractere {
+    public static boolean EhMaiusculo(char c){
+        if(Character.isUpperCase(c)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
