602c17e9bc30296a2459b5dfc9aeca6e79962205
juniorshait <juniorshait@gmail.com>
2022-05-02 08:34:19 -0700

    Escreva um código em java que imprima os números de 0 a 99

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,8 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+            for(int i =0; i<99; i++){
+                System.out.printf("%d%s",i, ", ");
+            }
+                System.out.printf("%d\n",99);
+        }
+    }
