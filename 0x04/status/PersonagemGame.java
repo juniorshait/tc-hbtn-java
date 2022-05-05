4af642463136228db4b0ec343eac0f9f27134c3d
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-05 16:53:18 -0300

    Add files via upload

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,52 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+
+    private String status;
+
+    public void tomarDano(int quantidadeDeDano){
+        int saude = getSaudeAtual() - quantidadeDeDano;
+        if(saude <= 0){
+            this.saudeAtual = 0;
+        }else{
+            this.saudeAtual = saude;
+        }
+
+    }
+    public void receberCura(int quantidadeDeCura){
+        int cura = getSaudeAtual() + quantidadeDeCura;
+        if(cura > 100){
+            this.saudeAtual = 100;
+        }else{
+            this.saudeAtual = cura;
+        }
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        if(getSaudeAtual() == 0){
+           status = "morto";
+        }else{
+            status = "vivo";
+        }
+        return status;
+    }
+
+}
