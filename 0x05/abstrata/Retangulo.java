3cdab209c5c5811e63fb99519ad9b365a100f274
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-05 18:55:30 -0300

    Add files via upload

---
+++ 0x05/abstrata/Retangulo.java
@@ -0,0 +1,38 @@
+public class Retangulo extends FormaGeometrica{
+
+    protected double largura;
+    protected double altura;
+
+    public String toString(){
+        return String.format("[Retangulo] %.2f / %.2f",largura,altura);
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if(largura >=0 ){
+            this.largura = largura;
+        }else{
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if(altura >=0 ){
+            this.altura = altura;
+        }else{
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+}
