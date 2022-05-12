958c00a67717b9e2ba630863e36adb0a213e659d
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-12 17:04:31 -0300

    Add files via upload

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,45 @@
+public class Empregado {
+
+   private String nome;
+   private int codigo;
+   private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Empregado(String nome, int codigo, Endereco endereco){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public void apresentar(){
+        System.out.println("Codigo: "+this.codigo+"" +
+                "\nNome: "+this.nome+
+                "\nBairro: "+this.endereco.getBairro()+
+                "\nCidade: "+this.endereco.getCidade()+
+                "\nPais: "+this.endereco.getPais()
+        );
+    }
+}
