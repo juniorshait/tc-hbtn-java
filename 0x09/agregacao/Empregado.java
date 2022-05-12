bfe684c6e78dc9646d21f7d23d17dfd29d6c0f75
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-12 17:18:32 -0300

    Add files via upload

--- 0x09/agregacao/Empregado.java
+++ 0x09/agregacao/Empregado.java
@@ -39,6 +39,6 @@ public class Empregado {
         System.out.println("Nome: "+this.nome);
         System.out.println("Bairro: "+this.endereco.getBairro());
         System.out.println("Cidade: "+this.endereco.getCidade());
-        System.out.println("Pais: "+this.endereco.getPais());
+        System.out.print("Pais: "+this.endereco.getPais());
     }
 }
