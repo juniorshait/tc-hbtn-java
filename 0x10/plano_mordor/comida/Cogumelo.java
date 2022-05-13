cffc90a971177dc5b3aec2d2a975432858ba80b1
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-13 17:59:38 -0300

    Add files via upload

---
+++ 0x10/plano_mordor/comida/Cogumelo.java
@@ -0,0 +1,9 @@
+package comida;
+
+public class Cogumelo extends Comida{
+
+    @Override
+    public int getPontosDeFelicidade() {
+        return -10;
+    }
+}
---
+++ 0x10/plano_mordor/comida/Comida.java
@@ -0,0 +1,10 @@
+package comida;
+
+public abstract class Comida {
+    public Comida() {
+
+    }
+
+    public abstract int getPontosDeFelicidade();
+
+}
