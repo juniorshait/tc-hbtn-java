0322fc5831b9b50ba2b1f4b95e871f08a680425f
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-20 13:17:20 -0300

    Add files via upload

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -4,7 +4,9 @@ public class AnalisadorFrase {
 
     public static TreeMap contagemPalavras(String frase){
         String minuscula = frase.toLowerCase();
-        minuscula = minuscula.replaceAll("\\p{Punct}", "");
+        minuscula = minuscula.replaceAll("!","");
+        minuscula = minuscula.replaceAll("\\.","");
+        minuscula = minuscula.replaceAll("\\?","");
         String[] palavras = minuscula.split(" ");
         Set<String> chaves = new HashSet<>(List.of(palavras));
         TreeMap<String, Integer> repete = new TreeMap<>();
