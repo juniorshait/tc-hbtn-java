c062f121cb927b418df2ed2c2244deabedb67868
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-26 17:14:04 -0300

    Add files via upload

---
+++ Generics_exercises/armazem_especifico/Armazem.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Armazem<T> implements Armazenavel<T>{
+
+    Map<String, T> armazem = new HashMap<>();
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+       armazem.put(nome, valor);
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+        return armazem.get(nome);
+    }
+}
