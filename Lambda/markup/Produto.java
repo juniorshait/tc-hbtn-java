bc49769dbf136a40de867bf5b20ff9d99856a0f4
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-05-30 11:05:04 -0300

    Add files via upload

---
+++ Lambda/markup/Produto.java
@@ -0,0 +1,43 @@
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private double percentualMarkUp = 10;
+
+    Supplier<Double> precoComMarkUp = () -> this.preco + (this.preco*percentualMarkUp/100);
+    Consumer<Double> atualizarMarkUp = valor -> {
+        this.percentualMarkUp = valor;
+    };
+
+    public Produto(double valor, String nome) {
+        this.nome = nome;
+        this.preco = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getPercentualMarkUp() {
+        return percentualMarkUp;
+    }
+
+    public void setPercentualMarkUp(double percentualMarkUp) {
+        this.percentualMarkUp = percentualMarkUp;
+    }
+
+}
