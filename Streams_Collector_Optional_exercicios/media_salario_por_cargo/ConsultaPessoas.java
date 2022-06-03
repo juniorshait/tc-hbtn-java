35a485fc8555d3451663ad1a80aa61a4b16d048e
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-06-03 10:54:54 -0300

    Add files via upload

---
+++ Streams_Collector_Optional_exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(groupingBy(Pessoa::getCargo, ()-> new TreeMap<>(Comparator.reverseOrder()),Collectors.toCollection(TreeSet::new)));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+        return todasPessoas.stream().collect(groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoa) {
+        return pessoa.stream().collect(groupingBy(Pessoa::getCargo, groupingBy(Pessoa::getIdade, Collectors.counting())));
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoa) {
+        return pessoa.stream().collect(groupingBy(Pessoa::getCargo, Collectors.averagingDouble(Pessoa::getSalario)));
+    }
+}
